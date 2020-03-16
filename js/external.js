d8fa510b9ef9ad4f34e854417ec38141cd6a9579
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-16 16:57:18 -0500

    started js exercise

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log('Hello from external Javascript');
+
+alert('Welcome to my website!');
+
+var userFavColor = prompt('What is your favorite color?');
+
+alert('Great, ' + userFavColor  + ' is my favorite too!');
