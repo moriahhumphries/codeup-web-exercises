330c9f969908133ce458c01929a57e4ce6993d5f
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-18 18:51:18 -0500

    completed personal questions exercise

---
+++ js/personal-questions.js
@@ -0,0 +1,7 @@
+var firstName = prompt('What is your first name?');
+
+var lastName = prompt('What is your last name?');
+
+var yourAge = prompt('What is your age?');
+
+console.log('Your name is ' + firstName + " " + lastName + ' and you are ' + yourAge + ' years old.');
