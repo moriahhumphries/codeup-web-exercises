472fe75baa28866490cfcc8386cc66ac86d8b8c0
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-24 14:06:47 -0500

    first solution for while

--- js/while.js
+++ js/while.js
@@ -1,14 +1,27 @@
-var num = 2;
-
-while (num <= 65536) {
-    console.log(num);
-    num *= 2;
-}
+// var i = 2;
+//
+// while (i <= 65536) {
+//     console.log(i);
+//     i *= 2;
+// }
 
 // Ice Cream
 
-// var allCones = Math.floor(Math.random() * 50) + 50;
-//
-// while (Math.floor(Math.random() * 5) + 1) {
-//     console.log(allCones + " cones sold")
-// }
+var allCones = Math.floor(Math.random() * 50) + 50;
+var soldCones = Math.floor(Math.random() * 5) + 1;
+
+do {
+    console.log(allCones);
+    // var soldCones = Math.floor(Math.random() * 5) + 1;
+    console.log(soldCones + " cones sold");
+
+    console.log(allCones - soldCones);
+    allCones--;
+} while (soldCones < allCones);
+console.log('I am out of cones');
+
+
+
+
+
+
