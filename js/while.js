47b61fb9a9f58d9bcfe7e451bd969e928e330ff4
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-23 22:28:15 -0500

    started loops exercise

---
+++ js/while.js
@@ -0,0 +1,14 @@
+var num = 2;
+
+while (num <= 65536) {
+    console.log(num);
+    num *= 2;
+}
+
+// Ice Cream
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//
+// while (Math.floor(Math.random() * 5) + 1) {
+//     console.log(allCones + " cones sold")
+// }
