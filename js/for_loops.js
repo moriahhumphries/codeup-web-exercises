03b0d37dd1ec25e464d2ed76b846212c9fa50233
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-25 12:04:22 -0500

    complted for loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -9,7 +9,7 @@ function showMultiplicationTable(input) {
 
 showMultiplicationTable(7);
 
-// 10 random numbers between 20 and 200
+// #3 10 random numbers between 20 and 200
 
 for (var i = 0; i < 10; i++) {
 var randomNumber = Math.floor(Math.random() * 180) + 20;
@@ -22,28 +22,9 @@ console.log(randomNumber + ' is even');
 }
 }
 
-
-
-
-
-
-// var numberOfEvenNumbersFound = Math.floor(Math.random() * 200) + 20;
-// var numberOfOddNumbersFound = Math.floor(Math.random() * 200) + 20;
-// for (var i = 20; i <= 5; i++) {
-//     if (i % 2 === 0) {
-//         console.log('This number is even: ' + i);
-//         numberOfEvenNumbersFound++;
-//     } else if (i % 2 !== 0) {
-//         console.log('This number is odd: ' + i);
-//         numberOfOddNumbersFound++;
-//     }
-// }
-
-// #3 Create a for loop and the code from previous lesson to generate 10 random numbers between 20 and 200 and output odd or even.
-
 // #4 Create a for loop to show output
 
-for(i = 1; i < 8; i++) {
+for(i = 1; i <= 9; i++) {
     console.log(String(i).repeat(i))
 }
 
