7d92ca05d1f1d8e89aec4ca0af577616cf021817
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-24 21:22:18 -0500

    working on break exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,18 @@
-// var userNumber = prompt('Enter an odd number between 1 and 50: '); {
-//
-//
-//     console.log(userNumber);
-// }
+var random = (Math.floor((Math.random()*50)+1));
+
+console.log('Random odd number to skip: ' + random);
+
+
+for (var i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        continue;
+    }
+    if (random === i) {
+        console.log('Skipped number: ' + i);
+    } else {
+        console.log('Odd number: ' + i);
+    }
+    if (i >= 49) {
+        break;
+    }
+}
