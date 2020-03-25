d2ee253fe2f7eb96187a13a77d56a4ea3e0dbf9f
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-25 13:52:15 -0500

    updated all exercises

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,3 +1,21 @@
+
+
+
+
+// do {
+//     var userNumber = (prompt('Please enter an odd number between 1 and 50'));
+//     if (userNumber < 1 || userNumber > 50) {
+//         alert(userNumber + ' is not between 1 and 50');
+//     } else if (userNumber % 2 === 0) {
+//         alert(userNumber + ' is not a valid odd number');
+//     } else if (isNaN(userNumber)) {
+//         alert(userNumber + ' is not a number'); {
+//
+//         }
+//
+//     }
+// } while (true);
+
 var random = (Math.floor((Math.random()*50)+1));
 
 console.log('Random odd number to skip: ' + random);
