e34ab6aaef2065068fd73b6f993605a336e7872b
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-25 11:32:37 -0500

    updated while loops

--- js/while.js
+++ js/while.js
@@ -1,24 +1,31 @@
-// var i = 2;
-//
-// while (i <= 65536) {
-//     console.log(i);
-//     i *= 2;
-// }
+var i = 2;
+
+while (i <= 65536) {
+    console.log(i);
+    i *= 2;
+}
 
 // Ice Cream
 
 var allCones = Math.floor(Math.random() * 50) + 50;
-var soldCones = Math.floor(Math.random() * 5) + 1;
+
 
 do {
-    console.log(allCones);
-    // var soldCones = Math.floor(Math.random() * 5) + 1;
-    console.log(soldCones + " cones sold");
+    var soldCones = Math.floor(Math.random() * 5) + 1;
 
-    console.log(allCones - soldCones);
-    allCones--;
-} while (soldCones < allCones);
-console.log('I am out of cones');
+    if (soldCones < allCones) {
+       // Can Sell
+        console.log(soldCones + ' cones sold');
+        allCones -= soldCones;
+    } else {
+        // Can't Sell, not enough
+        console.log('Cannot sell you ' + soldCones + ' cones, I only have ' + allCones);
+    }
+} while (allCones > soldCones);
+
+    console.log('All cones have been sold!');
+
+
 
 
 
