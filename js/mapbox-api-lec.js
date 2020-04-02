8e4143212548820f1ae19c57ce8bd2340ce8504e
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-02 11:44:24 -0500

    added extra marker

--- js/mapbox-api-lec.js
+++ js/mapbox-api-lec.js
@@ -19,7 +19,11 @@ console.log('Token: ' + mapBoxToken);
 mapboxgl.accessToken = 'pk.eyJ1IjoibW9yaWFoaHVtcGhyaWVzIiwiYSI6ImNrOGl1Y2l1NTA0bTIzbG13MmFva2M5cTUifQ.LeM-tEB6EjF9glmzXdWrHg';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9'
+    style: 'mapbox://styles/mapbox/navigation-guidance-day-v4',
+    center: [-96.8057, 32.7787],
+    zoom: 16,
+    pitch: 25,
+
 });
 
 
@@ -39,6 +43,20 @@ var map = new mapboxgl.Map({
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-96.8084, 32.7799]. This marker will mark the Sixth Floor Muesume on our map.
 // TODO TOGETHER: Change the color of the marker
 
+var markerOptions = {
+    color: "orange",
+    draggable: true
+};
+
+var marker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.8057, 32.7787])
+    .addTo(map)
+
+var markerMuseum = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.8084, 32.7799])
+    .addTo(map)
+
+
 
 
 
@@ -57,11 +75,19 @@ var map = new mapboxgl.Map({
 
 // TODO TOGETHER: Add a popup to the map over codeup. Set the html as a paragraph that says "Codeup Rocks!"
 
+var popup = new mapboxgl.Popup()
+    .setHTML("<h1>Codeup Rocks!</h1>")
+    .addTo(map);
 
-
+marker.setPopup(popup);
 
 // TODO TOGETHER: Comment out the popup we just added. Add a popup to the Sixth Floor Musume marker.
 
+var popupMuseum = new mapboxgl.Popup()
+    .setHTML("<h1>Sixth Floor Museum</h1>")
+    .addTo(map);
+
+markerMuseum.setPopup(popupMuseum);
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
 // TODO: Try setting the text by using ".setText()" instead of ".setHTML()"
