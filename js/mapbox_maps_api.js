63cbbab7993e9275d7eff2aae2599911e2744a72
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-02 15:17:02 -0500

    started new mapbox exercise

---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,8 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW9yaWFoaHVtcGhyaWVzIiwiYSI6ImNrOGo2c283ZDA0azgzbHBxZGVtMWdzNjcifQ.Hoq9gRxqSSGKBQ_zK17yog';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center:
+});
+
