791681e5011732741ee42744a54df9cbaecf30eb
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-03 10:29:41 -0500

    added 2 restaurants

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -4,7 +4,7 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
     center: [-96.839450, 32.951550],
-    zoom: 13,
+    zoom: 11,
 
 });
 
@@ -13,8 +13,16 @@ var markerOptions = {
 
 };
 
-var marker = new mapboxgl.Marker(markerOptions)
+var chiMarker = new mapboxgl.Marker(markerOptions)
     .setLngLat([-96.839450, 32.951550])
+    .addTo(map);
+
+var pluMarker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.830040, 32.968610])
+    .addTo(map);
+
+var zoeMarker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.802800, 32.895770])
     .addTo(map);
 
 map.on('click', function () {
@@ -22,5 +30,22 @@ map.on('click', function () {
         .setHTML("<h3>Taste of Chicago</h3>")
         .addTo(map);
 
-    marker.setPopup(favoriteRestaurant);
+    chiMarker.setPopup(favoriteRestaurant);
+});
+
+
+map.on('click', function () {
+    var secondRestaurant = new mapboxgl.Popup()
+        .setHTML("<h3>Pluckers</h3>")
+        .addTo(map);
+
+    pluMarker.setPopup(secondRestaurant);
+});
+
+map.on('click', function () {
+    var zoeRestaurant = new mapboxgl.Popup()
+        .setHTML("<h3>Zoe's Kitchen</h3>")
+        .addTo(map);
+
+    zoeMarker.setPopup(zoeRestaurant);
 });
