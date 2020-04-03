148eda461a4dbd25ac8c6931711a5d9acab3f779
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-03 09:42:55 -0500

    added click function to map marker

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -3,6 +3,24 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibW9yaWFoaHVtcGhyaWVzIiwiYSI6ImNrOGo2c283ZDA0a
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v9',
-    center:
+    center: [-96.839450, 32.951550],
+    zoom: 13,
+
 });
 
+var markerOptions = {
+    color: "orange",
+
+};
+
+var marker = new mapboxgl.Marker(markerOptions)
+    .setLngLat([-96.839450, 32.951550])
+    .addTo(map);
+
+map.on('click', function () {
+    var favoriteRestaurant = new mapboxgl.Popup()
+        .setHTML("<h3>Taste of Chicago</h3>")
+        .addTo(map);
+
+    marker.setPopup(favoriteRestaurant);
+});
