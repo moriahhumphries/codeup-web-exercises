02885bb6e16cdd30f7bb7cb583484592a12a8d2b
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-08 15:13:49 -0500

    completed class selectors section

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-    console.log("DOM has loaded!");
-
-});
+// $(document).ready(function() {
+//     console.log("DOM has loaded!");
+//
+// });
 
 // id selectors
 
-var alert1 = $('#great').html();
-alert(alert1);
-
-var alert2 = $('#cats').html();
-alert(alert2);
+// var alert1 = $('#great').html();
+// alert(alert1);
+//
+// var alert2 = $('#cats').html();
+// alert(alert2);
 
 //duplicate id did not select second id on page
 
 // class selectors
 
+$('.codeup').css('border', '1px solid red')
