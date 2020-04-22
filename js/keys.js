fe95b60eec2665c12a3f9cd8ff8f4d6fd89a1d02
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-22 12:05:50 -0500

    updated keys js gitignore

--- js/keys.js
+++ js/keys.js
@@ -3,3 +3,5 @@ var mapBoxToken = 'pk.eyJ1IjoibW9yaWFoaHVtcGhyaWVzIiwiYSI6ImNrOGl3bmUxdDBhc3AzbW
 var catToken = 'ba211652-c113-4473-8677-f5ed56f5385b';
 
 const weatherKey = '6f9aaa5a929da31eac6beadc3938513e';
+
+const gitHub = '0f2c4467d8d47a42edef35d08c5ae446956f4b92'
