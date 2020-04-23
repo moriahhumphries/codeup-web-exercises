73db2b4453a9477c0cce79146126ac7182717d02
Moriah Humphries <moriah.humphries@gmail.com>
2020-04-23 11:28:41 -0500

    completed filter and map questions

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+// filter - users with at least 3 languages
+
+let userLanguages = users.filter(users => users.languages.length >= 3);
+
+console.log(userLanguages);
+
+// map - create array of strings, each element is a users email address
+
+let userEmail = users.map(users => users.email);
+console.log(userEmail);
